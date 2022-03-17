e44971a5979a1187db5d335ee535126594816c99
trendt000104 <asouza@trendoperadora.com.br>
2022-03-17 15:34:31 -0300

    Navigation buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,4 +1,5 @@
 import './styles.css';
+import { Link } from "react-router-dom";
 
 function Form() {
     const movie = {
@@ -30,10 +31,14 @@ function Form() {
                         </select>
                     </div>
                     <div className="dsmovie-form-btn-container">
-                        <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
+                        <Link to="/">
+                            <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
+                        </Link>                        
                     </div>
                 </form >
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                </Link>
             </div >
         </div >
     );
